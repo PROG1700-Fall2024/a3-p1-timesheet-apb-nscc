d918ab9654891f317d520d02bf36111c5b705776
apb-nscc <W0487099@campus.nscc.ca>
2024-11-12 17:44:09 -0400

    Pre-Ready for Marking

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -6,64 +6,85 @@
 #Student #:     W0487099
 #Student Name:  Alex Barr
 
-daysWorked = []
-daysWithMaxHours = []
-indexMaxHours = []
-maxHoursWorked = 0.0
-totalHoursWorked = 0.0
-averageHoursWorked = 0.0
-workDays = 5
-minShouldWork = 7
 
-dividerLength = 60
-
+#This function determines that only correct values can be inputted by the user
 def corrector(_minValue: int, _maxValue: int, _input: str, _errorMessage: str, _day):
-    if int(_input) < _minValue or int(_input) > _maxValue:
-        print(_errorMessage)
-        return corrector(_minValue, _maxValue, int(input(f"Enter hours worked on Day #{_day + 1}: ")), _errorMessage, _day)
-    else:
-        return _input
+    ### TRYING TRY/EXCEPT CODE I FOUND ON A YOUTUBE SHORT
+    while True:
+        try:
+            if int(_input) < _minValue or int(_input) > _maxValue:
+                print(_errorMessage)
+                return corrector(_minValue, _maxValue, (input(f"Enter hours worked on Day #{_day + 1}: ")), _errorMessage, _day)
+                break
+            else:
+                return int(_input)
+                break
+        except ValueError:
+            print(_errorMessage)
+            return corrector(_minValue, _maxValue, (input(f"Enter hours worked on Day #{_day + 1}: ")), _errorMessage, _day)
+            break
 
+#This function is part of the next function and it appends user information to a list; In this case hours worked to a list of days worked
 def dayHoursWorked(_day, _list):
-    _list.append(corrector(1,23,int(input(f"Enter hours worked on Day #{_day + 1}: ")),"That Number is Out of Range. Try Again.",_day))
+    _list.append(corrector(1,23,(input(f"Enter hours worked on Day #{_day + 1}: ")),"Value Invalid. Try Again.",_day))
 
+#This function prompts the user to input amount of hours worked per day, for as many days are in the workDays variable
 def EnterHoursWorked(_daysWorked: list, _daysWithMaxHours: list, _indexMaxHours: list, _workDays: int):
     for i in range(_workDays):
         dayHoursWorked(i, _daysWorked)
 
     for i in range(len(_daysWorked)):
-        if daysWorked[i] == max(_daysWorked):
-            daysWithMaxHours.append(max(_daysWorked))
+        if _daysWorked[i] == max(_daysWorked):
+            _daysWithMaxHours.append(max(_daysWorked))
             _indexMaxHours.append(i)
 
-def CalculateHoursWorked(_maxHoursWorked, _totalHoursWorked, _averageHoursWorked, _daysWorked, _workDays):
+#This takes information we have already received, typecasts it into either ints or floats, gets the max hours worked, gets the total numbers worked
+#and gets the average hours worked. Normally this would be done in three functions but I decided to try out multiple returns.
+def CalculateHoursWorked(_daysWorked, _workDays):
     _maxHoursWorked = int(max(_daysWorked))
     _totalHoursWorked = int(sum(_daysWorked))
     _averageHoursWorked = float(_totalHoursWorked / _workDays)
+    return _maxHoursWorked, _totalHoursWorked, _averageHoursWorked
 
+#This just executes code that tells the user relevant information such as total hours worked per week, average hours per week worked and the days
+#with the most hours worked. It presenting information that we viewing the code already have information on, but this code presents it to the user.
+def ShowProductiveDaysAndAverage(_daysWithMaxHours, _indexMaxHours, _averageHoursWorked, _totalHoursWorked, _dividerLength):
+    print("-"*_dividerLength)
+    print("The most hours worked on:")
+    for i in range(len(_daysWithMaxHours)):
+        print(f"Day #{_indexMaxHours[i] + 1} when you worked {_daysWithMaxHours[i]} hours.")
+    print("-"*_dividerLength)
 
+    print(f"The total number of hours worked was: {_totalHoursWorked}")
+    print(f"The average number of hours worked each day was: {_averageHoursWorked}")
+    print("-"*_dividerLength)
+
+#This code shows the days worked that are under 7 hours IF there are any days under 7 hours worked. Otherwise it doesn't show anything.
+def ShowSlackOffDays(_daysWorked, _minShouldWork, _workDays):
+    if min(_daysWorked) < 7:
+        print("Days you slacked off (i.e. worked less than 7 hours):")
+        for i in range(_workDays):
+            if _daysWorked[i] < _minShouldWork:
+                print(f"Day #{_daysWorked.index(_daysWorked[i]) + 1}: {_daysWorked[i]} hours ")
+ 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    
+    #Local Variable Initialization
+    daysWorked = []
+    daysWithMaxHours = []
+    indexMaxHours = []
+    maxHoursWorked = 0.0
+    totalHoursWorked = 0.0
+    averageHoursWorked = 0.0
+    workDays = 5
+    minShouldWork = 7
+    dividerLength = 60
 
     EnterHoursWorked(daysWorked,daysWithMaxHours,indexMaxHours,workDays)
-    CalculateHoursWorked(maxHoursWorked, totalHoursWorked, averageHoursWorked, daysWorked, workDays)
-
-
-    print("-"*dividerLength)
-    print("The most hours worked on:")
-    for i in range(len(daysWithMaxHours)):
-        print(f"Day #{indexMaxHours[i] + 1} when you worked {daysWithMaxHours[i]} hours.")
-    print("-"*dividerLength)
-
-    print(f"The total number of hours worked was: {totalHoursWorked}")
-    print(f"The average number of hours worked each day was: {averageHoursWorked}")
-    print("-"*dividerLength)
-
-    print("Days you slacked off (i.e. worked less than 7 hours):")
-
-    for i in range(workDays):
-        if daysWorked[i] < minShouldWork:
-            print(f"Day #{daysWorked.index(daysWorked[i]) + 1}: {daysWorked[i]} hours ")
+    maxHoursWorked, totalHoursWorked, averageHoursWorked = CalculateHoursWorked(daysWorked, workDays) #looked up how to get multiple values on stack overflow
+    ShowProductiveDaysAndAverage(daysWithMaxHours, indexMaxHours, averageHoursWorked, totalHoursWorked, dividerLength)
+    ShowSlackOffDays(daysWorked, minShouldWork, workDays)
 
     # YOUR CODE ENDS HERE
 
